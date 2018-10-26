f99a570e4929b91dfb87996a76503bf5fb589707
Nguyen Van Nam <namnv1@HaNT.local>
2018-10-26 18:00:03 +0700

    add vendor

---
+++ ci310/vendor/js/redirect.js
@@ -0,0 +1,42 @@
+
+//#################################################################
+// implementation of redirect buttons on the simulation html pages
+//#################################################################
+
+function myRedirectRing(){
+    window.location.href = "./ring";
+}
+
+function myRedirectOnramp(){
+    window.location.href = "./";
+}
+
+function myRedirectOfframp(){
+    window.location.href = "./offramp";
+}
+
+function myRedirectRoadworks(){
+    window.location.href = "./roadworks";
+}
+
+function myRedirectUphill(){
+    window.location.href = "./uphill";
+}
+
+function myRedirectRouting(){
+    window.location.href = "./routing";
+}
+
+function myRedirectRoundabout(){
+    window.location.href = "./roundabout";
+}
+
+function myRedirectGame(){
+    window.location.href = "./routingGame";
+}
+
+function myRedirectTrafficlight(){
+    window.location.href = "./trafficlight";
+}
+
+
